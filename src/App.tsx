3e5235a4bff0b2d21a2150a4d56ec4363b220d6f
CarlosWang <cw970516@gmail.com>
2023-06-30 09:43:01 +0800

    feat: realize the display of three charts

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { EChartsOption } from 'echarts'
+import * as echarts from 'echarts'
+import useMockData from './hooks/use-mock-data.ts'
+import dayjs, { ConfigType } from 'dayjs'
+import Chart from './components/chart'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { data, loading } = useMockData()
 
+  const group = 'group1'
+  const defaultOption: EChartsOption = {
+    title: {
+      text: 'ECharts example',
+      left: '0',
+      top: 'middle',
+      textAlign: 'center',
+    },
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: { type: 'line' },
+      formatter: (params: any) => {
+        const date = params[0].value[0]
+        const value = params[0].value[1]
+        return `Time: ${dayjs(date).format('YYYY/MM/DD HH:mm')}<br/>Rain: ${value}`
+      },
+    },
+    legend: {
+      data: ['Rain'],
+    },
+    xAxis: {
+      type: 'time',
+      axisLabel: {
+        formatter: (value: ConfigType) => {
+          return dayjs(value).format('HH:00')
+        },
+      },
+      splitNumber: 24,
+    },
+    yAxis: {},
+  }
+
+  const barChartOption: EChartsOption = {
+    ...defaultOption,
+    yAxis: {
+      ...defaultOption.yAxis,
+      inverse: true,
+    },
+    series: [
+      {
+        name: 'Rain',
+        type: 'bar',
+        data: data.map(i => [i.time, i.barData.toFixed(4)]),
+      },
+    ],
+  }
+
+  const lineChartOption: EChartsOption = {
+    ...defaultOption,
+    series: [
+      {
+        name: 'Rain',
+        type: 'line',
+        data: data.map(i => [i.time, i.lineData.toFixed(4)]),
+      },
+    ],
+  }
+
+  const scatterChartOption: EChartsOption = {
+    ...defaultOption,
+    series: [
+      {
+        name: 'Rain',
+        type: 'scatter',
+        data: data.map(i => [i.time, i.scatterData]),
+      },
+    ],
+  }
+
+  echarts.connect(group)
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <Chart option={barChartOption} loading={loading} group={group} />
+      <Chart option={lineChartOption} loading={loading} group={group} />
+      <Chart option={scatterChartOption} loading={loading} group={group} />
     </>
   )
 }
